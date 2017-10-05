d1b79d90ae1d73f9ece83ae825630524403e3752
Pavlo Kuchereshko <atos.ua@gmail.com>
2017-10-06 00:30:57 +0300

    tmdb

--- app/src/main/java/com/pk/tmdbapp/adapter/MoviesAdapter.java
+++ app/src/main/java/com/pk/tmdbapp/adapter/MoviesAdapter.java
@@ -2,16 +2,23 @@ package com.pk.tmdbapp.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.pk.tmdbapp.activities.DetailActivity;
 import com.pk.tmdbapp.R;
 import com.pk.tmdbapp.mvp.model.MovieModel;
@@ -34,7 +41,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
 
     @Override
     public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext())
+                View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.movie_card, viewGroup, false);
         return new MyViewHolder(view);
     }
@@ -46,14 +53,26 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
         viewHolder.userRating.setText(vote);
         String baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
-        Glide.with(mContext)
+                Glide.with(mContext)
                 .load(baseImageUrl + movieList.get(position).getPosterPath())
-                .apply(new RequestOptions()
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                viewHolder.progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                viewHolder.progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+                        })
+                /*.apply(new RequestOptions()
                         .placeholder(R.drawable.load)
-                        .error(R.drawable.load))
+                        .error(R.drawable.load))*/
                 .into(viewHolder.thumbnail);
     }
-
     @Override
     public int getItemCount() {
         return movieList.size();
@@ -64,12 +83,14 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
         TextView title;
         TextView userRating;
         ImageView thumbnail;
+        ProgressBar progressBar;
 
         MyViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
             userRating = (TextView) itemView.findViewById(R.id.user_rating);
             thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progress);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override

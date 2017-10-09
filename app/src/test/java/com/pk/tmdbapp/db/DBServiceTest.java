5d04697c0ac18be4b3eae11bf0ba7ab274d4a162
Pavlo Kuchereshko <atos.ua@gmail.com>
2017-10-09 23:17:20 +0300

    mvp

--- app/src/test/java/com/pk/tmdbapp/db/DBServiceTest.java
+++ app/src/test/java/com/pk/tmdbapp/db/DBServiceTest.java
@@ -1,5 +1,7 @@
 package com.pk.tmdbapp.db;
 
+import android.content.Context;
+
 import com.pk.tmdbapp.db.realmmodel.RealmMovie;
 
 import org.junit.After;
@@ -9,44 +11,59 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
 import io.reactivex.android.plugins.RxAndroidPlugins;
 import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
+import io.realm.internal.RealmCore;
+import io.realm.log.RealmLog;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
-import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 /**
  * Created by ace on 10/09/2017.
  */
 
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"org.mockito.*", "android.*"})
-@PrepareForTest({ RealmConfiguration.class, Realm.class, RealmQuery.class, RealmResults.class, RealmList.class, RealmObject.class })
+@PowerMockRunnerDelegate(RobolectricTestRunner.class)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@SuppressStaticInitializationFor("io.mockRealm.internal.Util")
+@PrepareForTest({Realm.class, RealmConfiguration.class, RealmQuery.class, RealmResults.class, RealmCore.class, RealmLog.class})
 public class DBServiceTest {
 
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
+    //@Rule
+    //public MockitoRule rule = MockitoJUnit.rule();
 
-    private Realm realm;
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
+    private Realm mockRealm;
+    private RealmResults<RealmMovie> realmMovies;
 
     private DBService dbService;
 
@@ -56,49 +73,92 @@ public class DBServiceTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
+        mockStatic(RealmCore.class);
+        mockStatic(RealmLog.class);
         mockStatic(Realm.class);
+        mockStatic(RealmConfiguration.class);
+        Realm.init(RuntimeEnvironment.application);
 
-        RealmConfiguration config =
-                new RealmConfiguration.Builder().inMemory().name("test-realm").build();
-        realm = Realm.getInstance(config);
+        final Realm mockRealm = mock(Realm.class);
+        final RealmConfiguration mockRealmConfig = mock(RealmConfiguration.class);
 
-        dbService = new DBService(realm);
+        doNothing().when(RealmCore.class);
+        RealmCore.loadLibrary(any(Context.class));
 
+        whenNew(RealmConfiguration.class).withAnyArguments().thenReturn(mockRealmConfig);
+        when(Realm.getDefaultInstance()).thenReturn(mockRealm);
+        when(mockRealm.createObject(RealmMovie.class)).thenReturn(new RealmMovie());
+
+        RealmMovie movie1 = new RealmMovie();
+        RealmMovie movie2 = new RealmMovie();
+        RealmMovie movie3 = new RealmMovie();
+        RealmMovie movie4 = new RealmMovie();
+
+        movie1.setTitle("Movie 1");
+        movie2.setTitle("Movie 2");
+        movie3.setTitle("Movie 3");
+        movie4.setTitle("Movie 4");
+
+        List<RealmMovie> movieList = new ArrayList<>();
+
+        movieList.add(movie1);
+        movieList.add(movie2);
+        movieList.add(movie3);
+        movieList.add(movie4);
+
+        RealmQuery<RealmMovie> realmMoviesQuery = mockRealmQuery();
+
+        when(realmMoviesQuery.findFirst()).thenReturn(movieList.get(0));
+
+        when(mockRealm.where(RealmMovie.class)).thenReturn(realmMoviesQuery);
+
+        when(realmMoviesQuery.equalTo(anyString(), anyInt())).thenReturn(realmMoviesQuery);
+
+        mockStatic(RealmResults.class);
+
+        RealmResults<RealmMovie> realmMovies = mockRealmResults();
+
+        when(mockRealm.where(RealmMovie.class).findAll()).thenReturn(realmMovies);
+
+        when(realmMoviesQuery.between(anyString(), anyInt(), anyInt())).thenReturn(realmMoviesQuery);
+
+        when(realmMoviesQuery.beginsWith(anyString(), anyString())).thenReturn(realmMoviesQuery);
+
+        when(realmMoviesQuery.findAll()).thenReturn(realmMovies);
+
+        when(realmMovies.iterator()).thenReturn(movieList.iterator());
+
+        when(realmMovies.size()).thenReturn(movieList.size());
+
+        /*RealmConfiguration config =
+                new RealmConfiguration.Builder().inMemory().name("test-mockRealm").build();
+        mockRealm = Realm.getInstance(config);*/
+
+        this.mockRealm = mockRealm;
+        this.realmMovies = realmMovies;
+        this.dbService = new DBService(mockRealm);
     }
 
     @BeforeClass
     public static void setUpClass() {
 
         // Override the default "out of the box" AndroidSchedulers.mainThread() Scheduler
-        //
-        // This is necessary here because otherwise if the static initialization block in AndroidSchedulers
-        // is executed before this, then the Android SDK dependent version will be provided instead.
-        //
-        // This would cause a java.lang.ExceptionInInitializerError when running the test as a
-        // Java JUnit test as any attempt to resolve the default underlying implementation of the
-        // AndroidSchedulers.mainThread() will fail as it relies on unavailable Android dependencies.
-
-        // Comment out this line to see the java.lang.ExceptionInInitializerError
         RxAndroidPlugins.setInitMainThreadSchedulerHandler(__ -> Schedulers.trampoline());
     }
 
     @Test
     public void save() throws Exception {
-        System.out.println(dbService);
-        System.out.println(realm);
-        /*when(dbService.save(realm, Mockito.any(RealmMovie.class), RealmMovie.class))
+        when(dbService.save(any(RealmMovie.class), RealmMovie.class))
                 .thenAnswer(invocationOnMock -> {
                     RealmMovie movie = invocationOnMock.getArgumentAt(0, RealmMovie.class);
                     movies.add(movie);
                     return null;
-                });*/
+                });
     }
 
     @Test
     public void remove() throws Exception {
-        /*System.out.println(dbService);
-        System.out.println(realm);
-        when(dbService.remove(realm, Mockito.any(RealmMovie.class)))
+        /*when(dbService.remove(mockRealm, Mockito.any(RealmMovie.class)))
                 .thenAnswer(invocationOnMock -> movies.get(invocationOnMock.getArgument(0)));*/
     }
 
@@ -112,15 +172,23 @@ public class DBServiceTest {
 
     }
 
+    @SuppressWarnings("unchecked")
+    private <T extends RealmObject> RealmQuery<T> mockRealmQuery() {
+        return mock(RealmQuery.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends RealmObject> RealmResults<T> mockRealmResults() {
+        return mock(RealmResults.class);
+    }
+
     @After
     public void tearDown() throws Exception {
-        realm.close();
+        mockRealm.close();
     }
 
     @AfterClass
     public static void tearDownClass() {
-        // Not strictly necessary because we can't reset the value set by setInitMainThreadSchedulerHandler,
-        // but it doesn't hurt to clean up anyway.
         RxAndroidPlugins.reset();
     }
 
